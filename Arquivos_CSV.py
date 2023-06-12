bedc81a26e8c2fc5d8a9f68ed05221279ac7e875
Thales Lopes <thalesrslopes@gmail.com>
2023-06-11 22:30:19 -0300

    Escrevendo em arquivos csv

--- Arquivos_CSV.py
+++ Arquivos_CSV.py
@@ -1,4 +1,5 @@
-from csv import reader, DictReader
+from csv import reader, DictReader, writer, DictWriter
+
 
 print("Utilizando reader")
 # faz a leitura do arquivo e retorna uma lista (iterator)
@@ -17,3 +18,43 @@ with open('files\\lutadores.csv', encoding='utf-8') as arquivo:
     leitor = DictReader(arquivo, delimiter=',')
     for linha in leitor:
         print(f'{linha["Nome"]} nasceu em {linha["País"]} e tem {linha["Altura (em cm)"]} centímetros de altura')
+
+
+print("\n\nEscrevendo dados no arquivo csv utilizando Writer")
+with open('files\\filmes.csv', 'w', encoding='utf-8') as arquivo:
+    escritor = writer(arquivo)
+    # Escreve o cabeçalho
+    escritor.writerow(["Título", "Gênero", "Duração"])
+    # Escrevendo os filmes
+    escritor.writerow(["O Senhor dos Anéis: A Sociedade do Anel", "Fantasia", "233"])
+    escritor.writerow(["O Senhor dos Anéis: As duas Torres", "Fantasia", "245"])
+    escritor.writerow(["O Senhor dos Anéis: O Retorno do Rei", "Fantasia", "257"])
+
+print("\nOs dados escritos no arquivo foram:")
+# faz a leitura do arquivo e retorna um dicionário
+# onde cada linha é um dicionário (o conteúdo é uma lista de dicionários)
+with open('files\\filmes.csv', encoding='utf-8') as arquivo:
+    leitor = DictReader(arquivo, delimiter=',')
+    for linha in leitor:
+        print(f'O filme {linha["Título"]} é do gênero {linha["Gênero"]} e tem {linha["Duração"]} minutos de duração')
+
+
+print("\n\nEscrevendo dados no arquivo csv utilizando DictWriter")
+with open('files\\filmes2.csv', 'w', encoding='utf-8') as arquivo:
+    cabecalho = ["Título", "Gênero", "Duração"]
+    escritor = DictWriter(arquivo, fieldnames=cabecalho)
+    # Escreve o cabeçalho
+    escritor.writeheader()
+    # Escrevendo os filmes
+    escritor.writerow({"Título": "O Senhor dos Anéis: A Sociedade do Anel", "Gênero": "Fantasia", "Duração": "233"})
+    escritor.writerow({"Título": "O Senhor dos Anéis: As duas Torres", "Gênero": "Fantasia", "Duração": "245"})
+    escritor.writerow({"Título": "O Senhor dos Anéis: O Retorno do Rei", "Gênero": "Fantasia", "Duração": "257"})
+
+
+print("\nOs dados escritos no arquivo foram:")
+# faz a leitura do arquivo e retorna um dicionário
+# onde cada linha é um dicionário (o conteúdo é uma lista de dicionários)
+with open('files\\filmes2.csv', encoding='utf-8') as arquivo:
+    leitor = DictReader(arquivo, delimiter=',')
+    for linha in leitor:
+        print(f'O filme {linha["Título"]} é do gênero {linha["Gênero"]} e tem {linha["Duração"]} minutos de duração')
